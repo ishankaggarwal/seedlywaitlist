0c7dfe638da3ce476fe7bf3b2745b5188f20fb78
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-10 22:37:17 -0800

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./providers";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Seedly",
   description:
-    "Due Diligence without the grunt work. Use your data room to pull all the info you need for analysis.",
+    "Due Diligence without the grunt work. Use your data room to pull all the info you need for analysis. Gather data for analysis in minutes, not weeks. Every investment memo starts with the same tedious research. We pull that data so you can jump into the real work, right away.",
+  icons: {
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -18,9 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
       <body>
         <Providers>
           <div>

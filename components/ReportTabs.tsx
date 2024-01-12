6c86a2de83a910d7eab87db0b0326f06ad507b30
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-12 11:10:47 -0800

    Fixed Public comparables typo

--- components/ReportTabs.tsx
+++ components/ReportTabs.tsx
@@ -4,13 +4,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { ReactElement, useState } from "react";
 import Image from "next/image";
-import { 
-  UserIcon, 
-  BuildingOfficeIcon, 
-  BuildingOffice2Icon, 
-  ArrowTrendingUpIcon, 
-  TableCellsIcon 
-} from "@heroicons/react/20/solid"
+import {
+  UserIcon,
+  BuildingOfficeIcon,
+  BuildingOffice2Icon,
+  ArrowTrendingUpIcon,
+  TableCellsIcon,
+} from "@heroicons/react/20/solid";
 
 interface Tab {
   id: string;
@@ -49,7 +49,7 @@ export default function ReportTabs() {
     {
       id: "public-comparables",
       label: "Public Comparables",
-      image: "/public-comparables.png",
+      image: "/public-comparables.svg",
       icon: <TableCellsIcon className={iconClasses} />,
     },
   ];
